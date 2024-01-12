b59c8fa56649e46fc3ae97a92eb238eb7f855b38
abdallhahamdy <abdallhahamdy@yahoo.com>
2024-01-12 22:50:43 +0200

    init

---
+++ src/main/java/com/javatechie/bookingliquibase/BookingLiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.javatechie.bookingliquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingLiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingLiquibaseApplication.class, args);
+	}
+
+}
